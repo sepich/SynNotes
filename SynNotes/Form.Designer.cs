80f898f05763b1804d90d7ecb38d24a9bd22ab2b
sepa <i@sepa.spb.ru>
2014-12-28 11:56:46 +0300

    tray icon and menu

--- SynNotes/Form.Designer.cs
+++ SynNotes/Form.Designer.cs
@@ -29,34 +29,39 @@
         private void InitializeComponent()
         {
       this.components = new System.ComponentModel.Container();
-      System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("Node1");
-      System.Windows.Forms.TreeNode treeNode29 = new System.Windows.Forms.TreeNode("Node2");
-      System.Windows.Forms.TreeNode treeNode30 = new System.Windows.Forms.TreeNode("Node3");
-      System.Windows.Forms.TreeNode treeNode31 = new System.Windows.Forms.TreeNode("Node4");
-      System.Windows.Forms.TreeNode treeNode32 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
-            treeNode28,
-            treeNode29,
-            treeNode30,
-            treeNode31});
-      System.Windows.Forms.TreeNode treeNode33 = new System.Windows.Forms.TreeNode("Node6");
-      System.Windows.Forms.TreeNode treeNode34 = new System.Windows.Forms.TreeNode("Node7");
-      System.Windows.Forms.TreeNode treeNode35 = new System.Windows.Forms.TreeNode("Node8");
-      System.Windows.Forms.TreeNode treeNode36 = new System.Windows.Forms.TreeNode("Node5", new System.Windows.Forms.TreeNode[] {
-            treeNode33,
-            treeNode34,
-            treeNode35});
+      System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node1");
+      System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node2");
+      System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node3");
+      System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node4");
+      System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4});
+      System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node6");
+      System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node7");
+      System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node8");
+      System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Node5", new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7,
+            treeNode8});
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
       this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
       this.statusStrip1 = new System.Windows.Forms.StatusStrip();
       this.splitContainer1 = new System.Windows.Forms.SplitContainer();
       this.treeView1 = new System.Windows.Forms.TreeView();
       this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.scEdit = new ScintillaNET.Scintilla();
       this.menuStrip1 = new System.Windows.Forms.MenuStrip();
       this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
       this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-      this.scEdit = new ScintillaNET.Scintilla();
+      this.contextMenuTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+      this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
       this.toolStripContainer1.BottomToolStripPanel.SuspendLayout();
       this.toolStripContainer1.ContentPanel.SuspendLayout();
       this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
@@ -65,8 +70,9 @@
       this.splitContainer1.Panel1.SuspendLayout();
       this.splitContainer1.Panel2.SuspendLayout();
       this.splitContainer1.SuspendLayout();
-      this.menuStrip1.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.scEdit)).BeginInit();
+      this.menuStrip1.SuspendLayout();
+      this.contextMenuTray.SuspendLayout();
       this.SuspendLayout();
       // 
       // toolStripContainer1
@@ -127,27 +133,27 @@
       this.treeView1.Indent = 19;
       this.treeView1.Location = new System.Drawing.Point(0, 21);
       this.treeView1.Name = "treeView1";
-      treeNode28.Name = "Node1";
-      treeNode28.Text = "Node1";
-      treeNode29.Name = "Node2";
-      treeNode29.Text = "Node2";
-      treeNode30.Name = "Node3";
-      treeNode30.Text = "Node3";
-      treeNode31.Name = "Node4";
-      treeNode31.Text = "Node4";
-      treeNode32.Name = "Node0";
-      treeNode32.Text = "Node0";
-      treeNode33.Name = "Node6";
-      treeNode33.Text = "Node6";
-      treeNode34.Name = "Node7";
-      treeNode34.Text = "Node7";
-      treeNode35.Name = "Node8";
-      treeNode35.Text = "Node8";
-      treeNode36.Name = "Node5";
-      treeNode36.Text = "Node5";
+      treeNode1.Name = "Node1";
+      treeNode1.Text = "Node1";
+      treeNode2.Name = "Node2";
+      treeNode2.Text = "Node2";
+      treeNode3.Name = "Node3";
+      treeNode3.Text = "Node3";
+      treeNode4.Name = "Node4";
+      treeNode4.Text = "Node4";
+      treeNode5.Name = "Node0";
+      treeNode5.Text = "Node0";
+      treeNode6.Name = "Node6";
+      treeNode6.Text = "Node6";
+      treeNode7.Name = "Node7";
+      treeNode7.Text = "Node7";
+      treeNode8.Name = "Node8";
+      treeNode8.Text = "Node8";
+      treeNode9.Name = "Node5";
+      treeNode9.Text = "Node5";
       this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode32,
-            treeNode36});
+            treeNode5,
+            treeNode9});
       this.treeView1.ShowLines = false;
       this.treeView1.Size = new System.Drawing.Size(219, 579);
       this.treeView1.TabIndex = 1;
@@ -160,49 +166,6 @@
       this.comboBox1.Name = "comboBox1";
       this.comboBox1.Size = new System.Drawing.Size(219, 21);
       this.comboBox1.TabIndex = 2;
-      // 
-      // menuStrip1
-      // 
-      this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
-      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
-      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-      this.menuStrip1.Name = "menuStrip1";
-      this.menuStrip1.Size = new System.Drawing.Size(1014, 24);
-      this.menuStrip1.TabIndex = 0;
-      this.menuStrip1.Text = "menuStrip1";
-      // 
-      // fileToolStripMenuItem
-      // 
-      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem,
-            this.toolStripMenuItem1,
-            this.exitToolStripMenuItem});
-      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-      this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-      this.fileToolStripMenuItem.Text = "File";
-      // 
-      // openToolStripMenuItem
-      // 
-      this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-      this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
-      this.openToolStripMenuItem.Text = "Open";
-      // 
-      // toolStripMenuItem1
-      // 
-      this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-      this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
-      // 
-      // exitToolStripMenuItem
-      // 
-      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-      this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
-      this.exitToolStripMenuItem.Text = "Exit";
-      // 
-      // notifyIcon1
-      // 
-      this.notifyIcon1.Text = "notifyIcon1";
-      this.notifyIcon1.Visible = true;
       // 
       // scEdit
       // 
@@ -237,18 +200,94 @@
       this.scEdit.Styles.Max.FontName = "Verdana\0\0\0\0\0\0\0\0\0\0\0\0\0";
       this.scEdit.TabIndex = 0;
       // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(1014, 24);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+      this.fileToolStripMenuItem.Text = "File";
+      // 
+      // openToolStripMenuItem
+      // 
+      this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+      this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.openToolStripMenuItem.Text = "Open";
+      // 
+      // toolStripMenuItem1
+      // 
+      this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+      this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+      // 
+      // exitToolStripMenuItem
+      // 
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.exitToolStripMenuItem.Text = "Exit";
+      // 
+      // notifyIcon1
+      // 
+      this.notifyIcon1.ContextMenuStrip = this.contextMenuTray;
+      this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+      this.notifyIcon1.Text = "SynNotes";
+      this.notifyIcon1.Visible = true;
+      this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+      // 
+      // contextMenuTray
+      // 
+      this.contextMenuTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem1});
+      this.contextMenuTray.Name = "contextMenuTray";
+      this.contextMenuTray.Size = new System.Drawing.Size(104, 54);
+      // 
+      // openToolStripMenuItem1
+      // 
+      this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+      this.openToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+      this.openToolStripMenuItem1.Text = "Show";
+      this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+      // 
+      // toolStripMenuItem2
+      // 
+      this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+      this.toolStripMenuItem2.Size = new System.Drawing.Size(100, 6);
+      // 
+      // exitToolStripMenuItem1
+      // 
+      this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+      this.exitToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+      this.exitToolStripMenuItem1.Text = "Exit";
+      this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(1014, 646);
       this.Controls.Add(this.toolStripContainer1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.MainMenuStrip = this.menuStrip1;
       this.Name = "Form1";
       this.Text = "SynNotes";
       this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
       this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
       this.Load += new System.EventHandler(this.Form1_Load);
+      this.Resize += new System.EventHandler(this.Form1_Resize);
       this.toolStripContainer1.BottomToolStripPanel.ResumeLayout(false);
       this.toolStripContainer1.BottomToolStripPanel.PerformLayout();
       this.toolStripContainer1.ContentPanel.ResumeLayout(false);
@@ -260,9 +299,10 @@
       this.splitContainer1.Panel2.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
       this.splitContainer1.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.scEdit)).EndInit();
       this.menuStrip1.ResumeLayout(false);
       this.menuStrip1.PerformLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.scEdit)).EndInit();
+      this.contextMenuTray.ResumeLayout(false);
       this.ResumeLayout(false);
 
         }
@@ -281,6 +321,10 @@
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private ScintillaNET.Scintilla scEdit;
+        private System.Windows.Forms.ContextMenuStrip contextMenuTray;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
     }
 }
 
--- SynNotes/Form.cs
+++ SynNotes/Form.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace SynNotes {
   
@@ -26,6 +27,7 @@ namespace SynNotes {
       if (File.Exists(confuserdir + conffile)) ini = new IniFile(confuserdir + conffile);
       else ini = new IniFile(conffile);
       this.WindowState = (FormWindowState)FormWindowState.Parse(this.WindowState.GetType(), ini.GetValue("Form", "WindowState", "Normal"));
+      this.Form1_Resize(this, null); //trigger tray icon
       if (this.WindowState == FormWindowState.Normal) {
         this.Top = Int32.Parse(ini.GetValue("Form", "Top", "100"));
         this.Left = Int32.Parse(ini.GetValue("Form", "Left", "100"));
@@ -56,5 +58,34 @@ namespace SynNotes {
       }
       ini.SetValue("Form", "WindowState", this.WindowState.ToString());
     }
+
+    [DllImport("user32.dll")] private static extern int ShowWindow(IntPtr hWnd, uint Msg);
+    private const uint SW_RESTORE = 0x09;
+
+    private void notifyIcon1_Click(object sender, EventArgs e) {
+      MouseEventArgs me = (MouseEventArgs)e;
+      if (me.Button == MouseButtons.Left) ShowWindow(this.Handle, SW_RESTORE);
+    }
+
+    private void Form1_Resize(object sender, EventArgs e) {
+      // show tray icon when minimized
+      if (this.WindowState == FormWindowState.Minimized) {
+        notifyIcon1.Visible = true;
+        this.ShowInTaskbar = false;
+      }
+      else if (notifyIcon1.Visible) {
+        notifyIcon1.Visible = false;
+        this.ShowInTaskbar = true;
+      }
+    }
+
+    private void exitToolStripMenuItem1_Click(object sender, EventArgs e) {
+      this.Close();
+    }
+
+    private void openToolStripMenuItem1_Click(object sender, EventArgs e) {
+      ShowWindow(this.Handle, SW_RESTORE);
+    }
+
   }
 }
